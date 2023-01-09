fc3f4680670812b6d90355135d5eaf5ba801bf4b
Adenis Ahmeti <105243644+adahmeti@users.noreply.github.com>
2023-01-09 16:58:55 +0100

    Update rating.c

--- Projects/rating.c
+++ Projects/rating.c
@@ -1,43 +1,82 @@
+ GNU nano 6.2                                   rating.c                                      M     
 #include <stdio.h>
 #include <string.h>
 int main(){
 
-int numMovies, rating, leastRating, favRating;
-char nameMovies[40], favorite[40], least[40];
+int i, numMovie, rating, favRating, leastRating, g;
+char nameMovie[40], favorite[40], least[40], name[40], surname[40],gender[3];
 
 favRating=0;
 leastRating=10;
 
-do{
-printf("PLease enter the how many movies have you watch this month: ");
-scanf(" %d", &numMovies);
 
-if(numMovies<1){
-printf("No movies! How you gonna rate them. Try again\n");
+
+printf("Hello\nThis is a program to see whats your favorite and least favorite movie");
+printf("But first\n");
+printf("Enter your name: ");
+scanf(" %s", name);
+printf("Enter your surname: ");
+scanf(" %s", surname);
+
+do{//start line for do
+printf("What is your gender?(For male press 1. For female press 2) \n");
+scanf(" %d", &g);
+if(g==1){
+strcpy(gender,"Mr");
 }
+if(g==2){
+strcpy(gender,"Ms");
 }
-while(numMovies<1);
-for(int i=0;i<numMovies;i++){
-printf("What is the name of the movie: (one word-title only)\n");
-scanf(" %s", nameMovies);
-printf("On a size from 1 to 10 how much you ganna rate it:\n");
-scanf(" %d", &rating);
+
+if(g>2){
+printf("You entered wrong input please try again\n");
+}
+}//end line for do
+
+while(g>2);
 
 
+do{//start line for do
+printf("\nHow many movies have you watched this month: ");
+scanf(" %d", &numMovie);
+if(numMovie<1){
+printf("No movies! HOw will you rate them\nTry again...\n");
+}
+}//end line for do
+while(numMovie<1);
+
+for(i=0;i<numMovie;i++){
+if(numMovie==1){
+printf("Whats the name of the movie: (One-Word title only)\n");
+scanf(" %s", nameMovie);
+printf("How much are you going to rate it: ");
+scanf(" %d", &rating );
+}
+if(numMovie>1){
+printf("Whats the name of the movies? (One-Word titles only) ");
+scanf(" %s", nameMovie);
+printf("How much are you going to rate it: ");
+scanf(" %d", &rating );
+}
 if(rating>favRating){
-strcpy(favorite, nameMovies);
+strcpy(favorite, nameMovie);
 favRating=rating;
+
 }
 
 if(rating<leastRating){
-strcpy(least, nameMovies);
+strcpy(least, nameMovie);
 leastRating=rating;
 }
 }
-printf("\nYour favorite move is %s.", favorite);
-printf("\nYour least favorite movie is %s.\n\n",least);
-return 0;
+if(numMovie==1){
+printf("For this month you only watchet one movie which is called %s and you rate it with %d.\n ", nameMovie, rating);
+}//In case that user has seen only one movies he will get this message
 
-
+if(numMovie>1){//In case that user has seen more than one movie it will show which movie he liked the most and which one the least
+printf("%s %c %c\n", gender, name[0], surname[0]);//In this line the program will print Mr or Ms depends on the gender user choosed
+printf("Your favorite movie is %s.\n", favorite);
 }
 
+return 0;
+}//end line
