3604b623131d669165b6b5d183ed5a7eac963be5
Adenis Ahmeti <105243644+adahmeti@users.noreply.github.com>
2023-01-12 22:08:46 +0100

    Update password.c

--- Projects/password.c
+++ Projects/password.c
@@ -1,50 +1,54 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
 
-int main(){//beggining line
-
+int main(){
 	int i;
+	i=7;
 	int hasUpper, hasLower, hasDigit;
-	char user[25], password[25];
+	char username[25], password[25];
 
-	hasUpper = hasLower=hasDigit=0;
+	printf("Enter your username: \n");
+	scanf(" %s", username);//user inputs his username
 
-   	printf("What is your username? \n");
-	scanf(" %s", user);
+	printf("Create a password: \n");
+	scanf(" %s", password);//User inputs the password
 
-	printf("Please create a password: \n");
-	scanf(" %s", password);
+	/*In this for loop it check if the password contains Upperletter lower letter and numbers
+	if in this case the passord contains what is needed the program will accept the password otherwise 
+	it will show a message to the user that their password shouild be changed 
+	and it has to cantain at least one Uppercase and a number also it has to be more than 8 digit
+	*/
 
-	for (i=0; i<strlen(password); i++){
+	for (int i = 0; i < strlen(password); ++i)
+	{
+
 		if(isdigit(password[i])){
 			hasDigit=1;
 			continue;
 		}
 		if(isupper(password[i])){
+
 			hasUpper=1;
 			continue;
 		}
 		if(islower(password[i])){
 			hasLower=1;
 		}
+		}
 
+		if ((hasDigit)&&(hasLower)&&(hasUpper)){
+			if(strlen(password)>i){//checks if the password is bigger than i in this case i is 7
+				//another way would be if the i would be 8 and it has to be written bigger or equal to i
+
+			printf("Your password is accepted\n");
+		}
+		}
+	else {
+		printf("Your password should contain Upper letter, number and at least 8 characters.\n");
+		//in case the password doesnt cointan the right credentials the user will see this message
 	}
-
-	if((hasDigit)&&(hasUpper)&&(hasLower)){
-
-		printf("\n\nExcelent work, %s, \n", user );
-		printf("Your password has uppper and lowercase letters and a number\n");
-	}
-	else{
-
-		printf("\nYou should consider a new password, %s,", user);
-		printf("\nOne that uses upper and lowercase letters nad a number\n\n");
-	}
-
 	
+return 0;
 
-
-return (0);
-
-}//end line 
+}
