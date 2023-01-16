9f6e8b65f0194b9f6875cbb47fe3c2681a89bdf6
Adenis Ahmeti <105243644+adahmeti@users.noreply.github.com>
2023-01-16 19:22:31 +0100

    Create mathhomework.c

---
+++ Projects/mathhomework.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main(){
+
+
+	printf("10 raised to the third power is %.0f.\n", pow(10.0, 3.0));
+	printf("The square root of 64 is %.0f.\n\n\n\n", sqrt(64) );
+
+	/******************************************************************/
+
+	printf("Its time to do the math homework :(!\n\n\n");
+	printf("Section 1: Square Roots\n\n");
+	printf("Square root of 49.00 is %.1f\n", sqrt(49.00));
+	printf("The square root of 149.0 is %.1f\n", sqrt(149.0));
+	printf("The square root of .149 is %.2f\n\n", sqrt(.149));
+
+	printf("Section 2: Powers\n\n");
+	printf("4 raised into 3rd power is %.1f\n", pow(4.0, 3.0) );
+	printf("7 raised into 5th power is %.1f\n", pow(7.0, 5.0));
+	printf("11 raised into 5th power is %.1f\n\n", pow(11.0, 5.0));
+
+	printf("Section 3: Trigonometry\n\n");
+	printf("The cosine of a 60-degree angle  is %.3f\n", cos((60*(3.14159/180.0))) );
+	printf("The sine of a 90-degree angle is %.3f\n", sin((90*(3.14159/180.0))));
+	printf("The tangent of a 75-degree angle is %.3f\n", tan((75*(3.14159/180.0))) );
+	printf("The arc cosine of a 45-degree angle is %.3f\n", acos((45*(3.14159/180.0))));
+	printf("The arc sine of a 30-degree angle is %.3f\n", asin(30*(3.14159/180.0)));
+	printf("The arc ttangent of a 69-degree angle is %.3f\n\n\n", atan(69*(3.14159/180.0)));
+
+	printf("Gucci !\n\n\n");
+
+	printf("================================================================================\n");
+	printf("Prigram enden homworks done\n");
+	return 0;
+}
